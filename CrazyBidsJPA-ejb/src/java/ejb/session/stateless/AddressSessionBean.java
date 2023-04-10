f051e1ccd64768b893137346a3031cd0d9420089
Kenneth-Mah <kennethmah@rocketmail.com>
2023-04-10 22:08:48 +0800

    Made changes to AddressEntitySessionBean, added createNewAddress() and retrieveAddressByAddressId() methods. Will not be adding retrieveAddressByAddressName() method as addressName attribute is not unique.

--- CrazyBidsJPA-ejb/src/java/ejb/session/stateless/AddressSessionBean.java
+++ CrazyBidsJPA-ejb/src/java/ejb/session/stateless/AddressSessionBean.java
@@ -5,7 +5,20 @@
  */
 package ejb.session.stateless;
 
+import entity.AddressEntity;
+import java.util.List;
+import java.util.Set;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import util.exception.AddressNotFoundException;
+import util.exception.InputDataValidationException;
+import util.exception.UnknownPersistenceException;
 
 /**
  *
@@ -14,6 +27,59 @@ import javax.ejb.Stateless;
 @Stateless
 public class AddressSessionBean implements AddressSessionBeanRemote, AddressSessionBeanLocal {
 
+    @PersistenceContext(unitName = "CrazyBidsJPA-ejbPU")
+    private EntityManager em;
+
+    private final ValidatorFactory validatorFactory;
+    private final Validator validator;
+    
+    public AddressSessionBean() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+
+    @Override
+    public Long createNewAddress(AddressEntity newAddressEntity) throws UnknownPersistenceException, InputDataValidationException {
+        Set<ConstraintViolation<AddressEntity>> constraintViolations = validator.validate(newAddressEntity);
+        
+        if (constraintViolations.isEmpty()) {
+            try {
+                em.persist(newAddressEntity);
+                em.flush();
+                
+                return newAddressEntity.getAddressId();
+            } catch (PersistenceException ex) {
+                throw new UnknownPersistenceException(ex.getMessage());
+            }
+        } else {
+            throw new InputDataValidationException(prepareInputDataValidationErrorsMessage(constraintViolations));
+        }
+    }
+    
+    @Override
+    public AddressEntity retrieveAddressByAddressId(Long addressId) throws AddressNotFoundException {
+        AddressEntity addressEntity = em.find(AddressEntity.class, addressId);
+        
+        if (addressEntity != null) {
+            return addressEntity;
+        } else {
+            throw new AddressNotFoundException("Address ID " + addressId + " does not exist!");
+        }
+    }
+    
+    // Not adding retrieveAddressByAddressName because addressName is not unique!
+    
+    private String prepareInputDataValidationErrorsMessage(Set<ConstraintViolation<AddressEntity>> constraintViolations) {
+        String msg = "Input data validation error!:";
+
+        for (ConstraintViolation constraintViolation : constraintViolations) {
+            msg += "\n\t" + constraintViolation.getPropertyPath() + " - " + constraintViolation.getInvalidValue() + "; " + constraintViolation.getMessage();
+        }
+
+        return msg;
+    }
+    
 }
--- CrazyBidsJPALibrary/src/ejb/session/stateless/AddressSessionBeanRemote.java
+++ CrazyBidsJPALibrary/src/ejb/session/stateless/AddressSessionBeanRemote.java
@@ -5,7 +5,11 @@
  */
 package ejb.session.stateless;
 
+import entity.AddressEntity;
 import javax.ejb.Remote;
+import util.exception.AddressNotFoundException;
+import util.exception.InputDataValidationException;
+import util.exception.UnknownPersistenceException;
 
 /**
  *
@@ -13,5 +17,9 @@ import javax.ejb.Remote;
  */
 @Remote
 public interface AddressSessionBeanRemote {
+    
+    public Long createNewAddress(AddressEntity newAddressEntity) throws UnknownPersistenceException, InputDataValidationException;
+    
+    public AddressEntity retrieveAddressByAddressId(Long addressId) throws AddressNotFoundException;
     
 }
